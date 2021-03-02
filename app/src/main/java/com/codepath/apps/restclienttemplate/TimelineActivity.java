8d36a7abbb16b9099088deb60b2f001e320d1d17
fzaman2258 <fzaman2258@bths.edu>
2021-03-02 17:46:46 -0500

    Added data persistence without internet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +15,9 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.models.TweetDao;
+import com.codepath.apps.restclienttemplate.models.TweetWithUser;
+import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -28,6 +32,7 @@ import okhttp3.Headers;
 public class TimelineActivity extends AppCompatActivity {
     private static final String TAG = "TimelineActivity";
     private final int REQUEST_CODE = 20;
+    TweetDao tweetDao;
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
@@ -41,6 +46,7 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
         client = TwitterApp.getRestClient(this);
 
+        tweetDao = ((TwitterApp) getApplicationContext()).getMyDatabase().tweetDao();
         swipeContainer = findViewById(R.id.swipeContainer);
 
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
@@ -75,6 +81,17 @@ public class TimelineActivity extends AppCompatActivity {
         //init the list of tweets and adapter
         // recycler view setup layout manager and adapter
 
+        AsyncTask.execute(new Runnable(){
+            @Override
+            public void run(){
+                Log.i(TAG,"Showing data from database");
+                List<TweetWithUser> tweetWithUsers = tweetDao.recentItems();
+                List<Tweet> tweetsFromDB = TweetWithUser.getTweetList(tweetWithUsers);
+                adapter.clear();
+                adapter.addAll(tweetsFromDB);
+            }
+        });
+
         populateHomeTimeline();
     }
 
@@ -103,7 +120,7 @@ public class TimelineActivity extends AppCompatActivity {
             Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
             tweets.add(0,tweet);
             adapter.notifyItemInserted(0);
-            rvTweets.smoothScrollToPosition(0); 
+            rvTweets.smoothScrollToPosition(0);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -137,9 +154,21 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.i(TAG,"onSuccess!" + json.toString());
                 JSONArray jsonArray = json.jsonArray;
                 try {
+                    final List<Tweet> tweetsFromNetwork = Tweet.fromJsonArray(jsonArray);
                     adapter.clear();
-                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    adapter.addAll(tweetsFromNetwork);
                     swipeContainer.setRefreshing(false);
+                    AsyncTask.execute(new Runnable(){
+                        @Override
+                        public void run(){
+                            Log.i(TAG,"Saving data into database");
+                            List<User> usersFromNetwork = User.fromJsonTweetArray(tweetsFromNetwork);
+                            tweetDao.insertModel(usersFromNetwork.toArray(new User[0]));
+                            tweetDao.insertModel(tweetsFromNetwork.toArray(new Tweet[0]));
+
+
+                        }
+                    });
                 } catch (JSONException e) {
                     Log.e(TAG,"Json exception",e);
                     e.printStackTrace();
